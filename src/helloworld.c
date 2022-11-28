8b040974c80c81c1e59ed40f53656b5c96dec4dd
Niyas Kunnakkadan <niyaskunnekkadan@gmail.com>
2022-11-28 10:41:37 +0530

    initialCommit

---
+++ src/helloworld.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : helloworld.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("-----------------------------\n");
+	printf("I write my first\n HELLO WORLD!!  \n");
+	printf("-----------------------------\n");/* prints !!!Hello World!!! */
+	return 0;
+}
+
+
+
+
